54206801e30eab1f942d3384c8a1e27520e7dd6a
zach-lau <zlau2000@gmail.com>
2022-04-03 12:51:39 -0600

    Add login page and register page

---
+++ src/pages/login.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Layout from "../components/layout";
+
+const LoginPage = () => {
+    return (
+        <div style={{position:"absolute", top:100, marginLeft: "auto", marginRight: "auto", width:"75%", left:0, right:0, textAlign: "center"}}>
+            <h1>Login</h1>
+            <p>We're not live yet but we hope to see you when we are!</p>
+        </div>
+    )
+}
+
+export default LoginPage;
---
+++ src/pages/register.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Layout from "../components/layout";
+
+const RegisterPage = () => {
+    return (
+        <div style={{position:"absolute", top:100, marginLeft: "auto", marginRight: "auto", width:"75%", left:0, right:0, textAlign: "center"}}>
+            <h1>Register</h1>
+            <p>We're not live yet but we hope to see you when we are!</p>
+        </div>
+    )
+}
+
+export default RegisterPage;
